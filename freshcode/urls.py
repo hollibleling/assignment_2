80f28d02714904f88b8671ff8e84f8b0459b19cb
fall031 <fall900802@gmail.com>
2021-11-06 01:32:01 +0900

    ADD: 상품 전체 목록 조회 기능

--- freshcode/urls.py
+++ freshcode/urls.py
@@ -17,26 +17,15 @@ from django.contrib import admin
 from django.urls import path, include
 from rest_framework import routers
 from rest_framework_nested.routers import NestedSimpleRouter
-from rest_framework_simplejwt.views import (
-    TokenObtainPairView,
-    TokenRefreshView,
-)
 
-from users.views import UserViewSet
-from products.views import ProductViewSet, ItemViewSet, TagViewSet
+from products.views import ProductViewSet
 
 router = routers.SimpleRouter(trailing_slash=False)
-router.register('user', UserViewSet, basename='user')
 router.register('product', ProductViewSet, basename='product')
 product = NestedSimpleRouter(router, 'product', lookup='product')
-product.register('item', ItemViewSet, basename='item')
-product.register('tag', TagViewSet, basename='tag')
-
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('', include(router.urls)),
     path('', include(product.urls)),
 ]
